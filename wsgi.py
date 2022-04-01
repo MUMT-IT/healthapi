2ac7574611bd27a96d55d5f059e5416fee77583d
Likit Preeyanon <likit.pre@mahidol.edu>
2022-04-01 10:51:16 +0700

    updated apis, and wsgi
    added fastcgi files

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
 from app import create_app
 from app.apis import apis
-from waitress import serve
 
 app = create_app()
 
-app.register_blueprint(apis, url_prefix='/apis')
+app.register_blueprint(apis, url_prefix='/api')
 
-serve(app, host='0.0.0.0', port=8080, threads=1)
+
+@app.route('/')
+def index():
+    return "Hello, from FastCGI"
+
+# app.register_blueprint(apis, url_prefix='/apis')
+
+# serve(app, host='0.0.0.0', port=8080, threads=1)
